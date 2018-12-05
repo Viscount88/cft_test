a9610fdb16e97c807cab651e05c1810dd70fe4ea
Alexandr <alebedko@retechlabs.com>
2018-12-05 18:46:20 +0700

    Initial Commit

---
+++ CFTTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CFTTest
+//
+//  Created by Александр Лебедько on 05/12/2018.
+//  Copyright © 2018 Денис Лебедько. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
